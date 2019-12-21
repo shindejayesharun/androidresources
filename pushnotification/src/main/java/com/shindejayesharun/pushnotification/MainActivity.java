0190340d8cfdc029131d031b11485e65b612c3f8
shindejayesharun <shindejayesharun@gmail.com>
2019-12-21 19:51:35 +0530

    push noti

--- pushnotification/src/main/java/com/shindejayesharun/pushnotification/MainActivity.java
+++ pushnotification/src/main/java/com/shindejayesharun/pushnotification/MainActivity.java
@@ -1,15 +1,55 @@
 package com.shindejayesharun.pushnotification;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "FIREBASE_SERVICE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.d(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+                        //String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, token);
+                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+
+                        Intent sendIntent = new Intent();
+                        sendIntent.setAction(Intent.ACTION_SEND);
+                        sendIntent.putExtra(Intent.EXTRA_TEXT, token);
+                        sendIntent.setType("text/plain");
+                        startActivity(sendIntent);
+
+                    }
+                });
+
+        Intent i=new Intent(this,MyService.class);
+        startService(i);
     }
 }
 
